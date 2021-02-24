da8b2bfb68836220416d467939828c719dd0b84c
Saizzou <65252447+Saizzou@users.noreply.github.com>
2021-02-24 17:47:40 +0100

    Update dispy.py

--- dispy.py
+++ dispy.py
@@ -1,106 +1,125 @@
-#bot gereksinimleri
 import discord
-import dispy_varlar
+import bilgi
 import random
 from discord.ext import commands
 from datetime import datetime
+import time
 
-TOKEN = dispy_varlar.TOKEN
-# TOKEN'i saklamak için TOKEN'i başka bir dosyadan çektim.
+
+TOKEN = bilgi.TOKEN
 
 client = discord.Client()
-bot = commands.Bot(command_prefix='!') #Client yerine Subclass olan bot'u çağırıyoruz ve '!' ile komut çağrısı yapıyoruz.
+bot = commands.Bot(command_prefix='!')
 
-with open('kotu_kelime.txt') as dosya: #Dosyayı doldurmanız gerekecek.
-    dosya = dosya.read().split()
-
-
-@bot.event #client.event yerine bot.event kullanıyoruz!
-async def on_ready():
-    print(f'{bot.user.name} bağlandı!')
-
-    for guild in bot.get_all_members():
-        print('Üye ve ID', end=":")
-        print(f'{guild}')
+with open('kufur.txt') as kufurler:
+    kufurler = kufurler.read().split()
 
 
+# Baglanilan Server'e ait Üye Bilgilerini Terminale aktar
 @bot.event
-async def on_member_join(member): #Kullanıcı katıldığında hoşgeldiniz mesajı oluşturur.
+async def on_ready():
+    print(f'{bot.user.name} baglandi!')
+
+    for server in bot.get_all_members():
+        print('Üye ve ID', end=":")
+        print(f'{server}')
+
+#Yeni Katilan Kullaniciya Hosgeldin Mesaji
+@bot.event
+async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-        f'Merhaba {member.name}, Discord Kanalımıza Hoşgeldin!'
+        f'Koddunyam Discord Kanalina Hosgeldin {member.name}! '
     )
 
+@bot.event
+async def giris_yapan_var(member):
+    await client.send_message(member, newUserDMMessage)
+    await client.send_message(discord.Object(id='CHANNELID'), 'Hosgeldin!! Sorulari http://forum.koddunyam.net üzerinden sorabilirsin! Burda da 10 Numara muhabbet dönüyor haberin olsun!!!')
 
-@bot.command(name='bilgi', help='Botun kurucu bilgisini verir.')
-#Ünlem (!komut) komut şeklinde çalışacak komutlar için kullanıyoruz örn: !bilgi
-
+#Bilgilendirme Komutu
+@bot.command(name='bilgi', help='Bot bilgilendirmesi')
 async def bilgi(ctx):
-    bilgi_cvp = "Bu Bot Saizzou tarafından yapılmıştır. Botun yapılışı için github.com/Saizzou/dispy_discord_botu \
-               adressine gidip botun kodlamasını inceleyebilirsiniz!"
-
+    bilgi_cvp = "Bu Bot Koddunyam Discord Kanalinin Karsilama Botudur!"
     await ctx.send(bilgi_cvp)
 
-@bot.command(name='zar_at', help='Zar atma simulasyonu !zar_at x y giriniz. Örneğin:'
-                               '!zar_at 2 6 şeklinde.')
-async def zar(ctx,zar_sayisi : int, zar_yuzu : int):
-    zarlar = [ str(random.choice(range(1,zar_yuzu+1)))
-               for _ in range(zar_sayisi)]
-    await ctx.send(','.join(zarlar))
+# !kufuret yazildiginda olacak olan islem:
+@bot.command(name='kufuret', help='Botun Kufur etmesini saglar')
+async def kufur(ctx):
+    kufur_cvp = "Seni tinkyler sonra winkylerim"
+    await ctx.send(kufur_cvp)
 
-'''Örnek olsun diye bu şekilde yaptım yoksa normalde çift zar için: 
-async def zar(ctx):
-    zarlar = [ str(random.choice(range(1,7)))
-               for _ in range(2)]
-    await ctx.send(','.join(zarlar))
-    şeklinde yapılabilirdi.'''
+# !zar-at denilince rastgele 2 zar ciktisi verir.
+@bot.command(name='zar-at', help='Zar oyunu')
+async def zar_at(ctx):
+    a = random.choice(range(1,7))
+    b = random.choice(range(1,7))
+    zar = f'Sansina cikan zarlar: {a} ve {b}'
+    await ctx.send(zar)
 
-@bot.command(name='kanal_olustur',help='Kanal olusturur.')
-@commands.has_role('Administrator')#Kanal oluşturma komutunu Administrator Grubuna verir.
-@commands.has_role('Editör') #Kanal oluşturma komutunu Editör Grubuna verir.
-async def kanal_olustur(ctx, yeni_kanal):
-    guild = ctx.guild
-    await guild.create_text_channel(yeni_kanal)
+# !topla sayi1 sayi2 ile toplama islemi yapmasini saglar
+@bot.command(name='topla',a=int,b=int,help="Toplama islemi yapmasini saglar")
+async def topla(ctx,a:int,b:int):
+    await ctx.send(a+b)
 
-@bot.command(name='kick', help='Birilerini atmak icin kullanilir')
+@bot.command()
+async def sec(ctx, *sec:str):
+    await ctx.send(random.choice(sec))
+
+# !metin_kanali komutu ile yeni metin kanali acar
+@bot.command(name='metin_kanali', help='Metin Kanali olusturur')
+@commands.has_role('Coder')
+async def metin_kanali(ctx, metin):
+    server = ctx.guild
+    await server.create_text_channel(metin)
+
+# !ses_kanali ile yeni ses kanali acar
+@bot.command(name='ses_kanali', help='Ses Kanali olusturur')
+@commands.has_role('Administrator')
+async def ses_kanali(ctx, ses):
+    server = ctx.guild
+    await server.create_voice_channel(ses)
+
+# Administrator yetkisi olan kullanicilarin !kick kullanici_adi komutu ile kullanici atmasini saglar
+@bot.command(name='kick', help= 'Birisini atmak icin kullan')
 @commands.has_role('Administrator')
 async def kick(ctx, userName: discord.User):
     await bot.kick(userName)
 
+
+# Bot'un konusmalarda olusacak olan mesaj iceriklerine cevap vermesini saglar
 @bot.event
-async def on_message(message):
-    kategoriler = ['python', 'genel-muhabbet'] #Botun olması istediğiniz kategorileri tam isimleriyle yazın.
-    if str(message.channel) in kategoriler:
+async def on_message(message): # Botun hangi kanallardaki konusmalara bakmasi gerektigini belirler 
+
+    if message.author == client.user:
+        return
+
+    selam_alma = ['Sa','Selam','s.a','sa','slm','selam']
+    bot_selamlama = ['Aleyna Aleyküm esselam', 'Ooooooo kimleri görüyorum', 'Vaaaaaay Sen yasiyor muydun ya?','Vay a.s Haci cav cav , nörüyon?', 'A.s ne geziyon buralarda?','Selamina selam cigerim!','Bana selam verme selam tutmayi... Yok yok konular karisti.. A.s','Dedi naber dedim iyidir... A.s genc!','Merhabalar, Koddunyam.net Discord Kanalina Hosgeldiniz... NÖRÜYON Cigerparem?']
+    
+    if 'mikael kim' in message.content.lower():
+        await message.channel.send('ADAM Kodun icinden geciyor öyle bi abimizdir! ')
+
+    if message.content.lower() in selam_alma:
+        selam_ver = random.choice(bot_selamlama)
+        await message.channel.send(selam_ver)
+
+    if 'bot' in message.content.lower():
+        await message.channel.send('Sssssss Bu konu forum.koddunyam.net harici tartisilmamalidir!')
         if message.author == client.user:
-            return  # Burası önemli bu kısmı unutursanız Bot kendi yazdığınada cevap verebilir.
+            return
 
-        selam_verme_ornekleri = ['Selam Server\'imize hoşgeldin', 'Ooo kimleri görüyoruz', 'Ve aleyküm selam',
-                                 'Merhabalar efenim', 'Birileri geri dönmüş bakıyorumda..', 'Dostum nerelerdeydin sen?']
+    elif 'özel ders' in message.content.lower():
+        await message.channel.send('ssssss Ozel ders Konusu acilinca Saizzou sövüyor!')
+        if message.author == client.user:
+            return
 
-        coklu = ['cok1', 'cok2', 'cok3'] #Buraya botun algılamasını istediğiniz kelimeleri girin
-        coklu_cevap = ['cevap1', 'cevap2', 'cevap3', 'cevap4'] #Buraya cevap seçenekleri girin
-
-        if message.content == 'selam':  # Bu kısım yüzde yüz bu şekilde olan mesaja cevap verir.
-            cevap = random.choice(selam_verme_ornekleri)
-            await message.channel.send(cevap)
-
-        elif 'sorum' in message.content.lower():  # Bu kısım cümle içinde sözcük geçerse cevap verir.
-            await message.channel.send('Sorularınız için Sorular kategorisine başvurabilirsiniz!')
-
-        elif message.content.lower() in coklu:  # Bu kısım çoklu kelime algılama ve çoklu cevap döndürmeye yarar.
-            cok_cevap = random.choice(coklu_cevap)
-            await message.channel.send(cok_cevap)
-
-        elif 'botu kim' in message.content.lower():
-            await message.channel.send('Bu botu Saizzou yaptı. Beğendiysen sanada yapsın... ')  # Reklam :)
-
-        for kotu_kelime in dosya:
-            if kotu_kelime in message.content.lower():
-                await message.delete()  # Kötü kelime barındıran mesajı siler
-                await message.channel.send(
-                    f'{message.author.mention}! Mesajınız kötü kelimeler yüzünden silinmiştir! UYARILDINIZ!')
+    for kufur in kufurler:
+        if kufur in message.content.lower():
+            await message.delete()
+            msg = f'{message.author.mention}! Kufur edeni tinkywinkylerim aklin basina gelir!'
+            await message.channel.send(msg)
 
 
-    await bot.process_commands(message)  # Bunu unutursanız message command'ı override eder ve command çalışmaz!!!
-
+    await bot.process_commands(message)
 bot.run(TOKEN)
